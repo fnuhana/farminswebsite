f8bc421359b3147943d044b54ef68bc3242c907a
farmin <nuha.farmin@gmail.com>
2025-02-25 20:07:59 -0500

    card name

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,29 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import '@mantine/core/styles.css';
+import { Card, Image, Text, Badge, Button, Group } from '@mantine/core';
+
+import { MantineProvider } from '@mantine/core';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
+  return <MantineProvider>
     <>
-      <div>
-      </div>
-      <h1>Hello</h1>
-      <div className="card">
-      </div>
+      <Card shadow="sm" padding="lg" radius="md" withBorder>
+      <Group justify="space-between" mt="md" mb="md">
+        <Text fw={900} c={'pink'}>hi my name is farmin nuhana!</Text>
+      </Group>
+
+      <Text size="sm" c="dimmed">
+      in my free time, i enjoy drawing/graphic design, traveling to new places, and drinking boba! 🧋
+      </Text>
+    </Card>
     </>
-  )
+    
+  </MantineProvider>
 }
 
 export default App
